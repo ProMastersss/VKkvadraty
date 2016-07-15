37becab35448dc21c138b1ae1b2c441feabbe836
ProMastersss <prokuror.93@yandex.ru>
2016-07-15 10:58:12 +0300

    Сделал активность друзей

--- auth.php
+++ auth.php
@@ -1,16 +1,15 @@
 <?php
 $CLIENT_ID ="5314962";
 $CLIENT_SECRET = "VYCmlu85eDMQNIrT069M";
-$ch = curl_init();
+
 $url = "https://oauth.vk.com/access_token?client_id=".$CLIENT_ID."&client_secret=".$CLIENT_SECRET."&v=5.52&grant_type=client_credentials";
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_exec($ch);
-curl_close($ch);
+
+$token = file_get_contents($url);
+$data = json_decode($token);
 
 $ch = curl_init();
-$url = "https://api.vk.com/method/secure.addAppEvent?user_id=31254554&activity_id=1&value=1&client_secret=".$CLIENT_SECRET."v=5.52";
+$url = "https://api.vk.com/method/secure.addAppEvent?user_id=".$_POST['id']."&activity_id=1&value=".$_POST['level']."&client_secret=".$CLIENT_SECRET."&access_token=".$data -> access_token."&v=5.52";
 curl_setopt($ch, CURLOPT_URL, $url);
-echo $_POST['token'];
-echo $data = curl_exec($ch);
+echo curl_exec($ch);
 curl_close($ch);
 ?>
